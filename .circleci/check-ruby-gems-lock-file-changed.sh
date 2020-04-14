eba17f2dca87b3b5ac94ae80073fe73bf02c2ff4
bguiz <bguiz@users.noreply.github.com>
2020-04-14 11:06:44 +0800

    test: dummy

--- .circleci/check-ruby-gems-lock-file-changed.sh
+++ .circleci/check-ruby-gems-lock-file-changed.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-git show $( git rev-parse master ):Gemfile.lock > Gemfile.master.lock
-CURRENT_LOCKFILE_HASH=$( sha256sum Gemfile.lock | cut -c "1-32" )
-PREVIOUS_LOCKFILE_HASH=$( sha256sum Gemfile.master.lock | cut -c "1-32" )
-if [ "${CIRCLE_BRANCH#proj/}" != "${CIRCLE_BRANCH}" -o "${CURRENT_LOCKFILE_HASH}" == "${PREVIOUS_LOCKFILE_HASH}" ] ; then
+COMMIT_HASH_MASTER=$( git merge-base origin/master HEAD )
+git show "${COMMIT_HASH_MASTER}":Gemfile.lock > Gemfile.master.lock
+diff -u Gemfile.master.lock Gemfile.lock > Gemfile.lock.diff
+LOCKFILE_DIFF_SIZE=$( wc -l < Gemfile.lock.diff )
+echo "COMMIT_HASH_MASTER=${COMMIT_HASH_MASTER}"
+echo "LOCKFILE_DIFF_SIZE=${LOCKFILE_DIFF_SIZE}"
+echo "CIRCLE_BRANCH=${CIRCLE_BRANCH}"
+if [ "${CIRCLE_BRANCH#proj/}" != "${CIRCLE_BRANCH}" -o "${LOCKFILE_DIFF_SIZE}" -eq 0 ] ; then
+  cat Gemfile.lock.diff
+  rm Gemfile.master.lock Gemfile.lock.diff
   echo "Gemfile.lock OK"
 else
-  echo "CURRENT_LOCKFILE_HASH=${CURRENT_LOCKFILE_HASH}"
-  echo "PREVIOUS_LOCKFILE_HASH=${PREVIOUS_LOCKFILE_HASH}"
-  echo "CIRCLE_BRANCH=${CIRCLE_BRANCH}"
   echo "Gemfile.lock has changed, and is not allowed except when branch name starts with proj/"
   echo "Changes to dependencies are a separate concern, and should be included in their own PR"
   exit 1
