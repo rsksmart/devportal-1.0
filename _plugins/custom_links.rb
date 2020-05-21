7fa20e7a4bac68a3222521c7b2aa29c21a56d880
bguiz <bguiz@users.noreply.github.com>
2020-05-21 17:21:29 +0800

    proj: custom link processing to make all URLs relative

--- _plugins/custom_links.rb
+++ _plugins/custom_links.rb
@@ -1,5 +1,5 @@
 require 'nokogiri'
-require 'uri'
+require 'pathname'
 
 # executed just after page and document rendering
 Jekyll::Hooks.register :pages, :post_render do |page_or_doc|
@@ -10,19 +10,34 @@ Jekyll::Hooks.register :documents, :post_render do |page_or_doc|
 end
 
 def customise_links!(p)
+  url = p.url
   dom = Nokogiri::HTML(p.output)
   dom.css('a').each do |a|
     href = a.get_attribute('href')
     if href =~ /\Ahttp(s)?:\/\//i
       # handle external link
       process_link_external!(a, href)
+    elsif href =~ /\A\//i
+      # handle internal absolute link
+      process_link_internal_absolute!(a, href, url)
     end
   end
   p.output = dom.to_s
 end
 
 def process_link_external!(a, href)
+  # adds additional attributes to external links to that
+  # they always open in a new page
   a.set_attribute('rel', 'external noopener noreferrer')
   a.set_attribute('target', '_blank')
-  puts a.to_s
+end
+
+def process_link_internal_absolute!(a, href, from_url)
+  # for example when the current page is at /foo/bar/baz
+  # and the link is for /quux/oof
+  # this should change the link to ../../quux/oof
+  from_path = Pathname.new(from_url.to_s)
+  to_path = Pathname.new(href.to_s)
+  relative_href = to_path.relative_path_from(from_path).to_s
+  a.set_attribute('href', relative_href)
 end
