6d280ff8bed41009b04f180580ae293f19be21cf
Owanate Amachree <amachreeowanate@gmail.com>
2023-11-06 16:27:23 +0200

    minor change

--- src/components/before-content/component-three/index.tsx
+++ src/components/before-content/component-three/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentThree = () => {
     return(
-        <a href="https://blog.rootstock.io/noticia/rsk-fingerroot-5-3-0-is-here/" target="_blank" rel="noopener noreferrer">  📢 RSKj Fingerroot 5.3.0 is here. 
+        <a href="https://blog.rootstock.io/noticia/rsk-fingerroot-5-3-0-is-here/" target="_blank" rel="noopener noreferrer">  📢 RSK Fingerroot 5.3.0 is here. 
       This version comes with improved client side JSON RPC, performance improvements, bug fixes and more. Read more!
       </a>
     )
