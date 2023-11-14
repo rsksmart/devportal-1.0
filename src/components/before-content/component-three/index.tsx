5461dc44f5ea022dbf63d28afaafcf998b8b8e78
Owanate Amachree <amachreeowanate@gmail.com>
2023-11-14 11:27:18 +0200

    added text spacing

--- src/components/before-content/component-three/index.tsx
+++ src/components/before-content/component-three/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ComponentThree = () => {
     return(
-        <a href="https://iovlabs.typeform.com/devportalsurvey" target="_blank" rel="noopener noreferrer">  Help us make the DevPortal best in class with your input. 
+        <a href="https://iovlabs.typeform.com/devportalsurvey" target="_blank" rel="noopener noreferrer">  Help us make the DevPortal best in class with your input. &nbsp;
         Take the Developer Portal Survey Now!
       </a>
     )
