a7f37386a71c71b6c22a6a1be93a4708368b1dec
Owanate Amachree <amachreeowanate@gmail.com>
2023-11-14 11:52:24 +0200

    minor change

--- src/components/before-content/component-three/index.tsx
+++ src/components/before-content/component-three/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ComponentThree = () => {
     return(
-        <a href="https://iovlabs.typeform.com/devportalsurvey" target="_blank" rel="noopener noreferrer">  Help us make the DevPortal best in class with your input. &nbsp;
-        Take the Developer Portal Survey Now!
+        <a href="https://iovlabs.typeform.com/devportalsurvey" target="_blank" rel="noopener noreferrer">  Help us make the DevPortal best in class with your input.
+        Take the Developer Portal Survey Now! &nbsp;
       </a>
     )
 }
