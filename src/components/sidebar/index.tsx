56acb376e2e1b49474982cb49e46b73610d261a5
Jonathan Smirnoff <jonathan.smirnoff@iovlabs.org>
2023-12-20 12:30:22 -0300

    chore: add prod env settings for mendable and set ask ai text

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -31,8 +31,8 @@ const Sidebar = ({ className, pathname }: Props) => {
       setExpanded
     }}>
       <div className={classnames(className, 'sidebar')}>
-        <div className="sidebar__wrapper">          
-          <MendableSearchBar anon_key={mendableApiKey} style={mendableSearchBarStyle} language="en" askAIText="Ask Rootstock AI" showSimpleSearch/>
+        <div className="sidebar__wrapper">
+          <Search />
           <div className="pr-4">
             <div className="my-2">
               <Link className="sidebar__link" to="/">Rootstock Documentation</Link>
