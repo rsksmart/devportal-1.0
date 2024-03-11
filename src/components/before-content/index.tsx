42ad31418960af54b6bedbd3b027f650fd4d1b97
Owanate Amachree <amachreeowanate@gmail.com>
2024-03-11 14:18:10 +0200

    Add Grants and Arrowhead 6.0.0 to before content

--- src/components/before-content/index.tsx
+++ src/components/before-content/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-// import Marquee from "react-fast-marquee";
 import classnames from "classnames";
+import Marquee from "react-fast-marquee";
 import ComponentOne from "./component-one";
+import ComponentTwo from "./component-two";
 
 interface Props {
   className?: string;
 }
 
 const BeforeContent = ({ className }: Props) => {
-  return (
-    <div className={classnames('before-content', className)}>      
-      <ComponentOne />
-    </div>
+  return ( 
+      <div className={classnames('before-content', className)}> 
+      <Marquee>    
+        <ComponentOne />
+        &nbsp;
+        <ComponentTwo />
+        &nbsp;
+      </Marquee>
+      </div> 
   );
 }
 
