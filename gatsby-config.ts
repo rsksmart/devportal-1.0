6b0e8b20511f9cb99af8b9bb8f320467e028b4dd
max-barrera-2148 <max.barrera@iovlabs.org>
2024-04-22 16:11:47 -0500

    Added robots.txt and excluded rif/identity

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -22,6 +22,15 @@ const config: GatsbyConfig = {
     `gatsby-transformer-json`,
     `gatsby-plugin-netlify`,
     //`gatsby-plugin-no-index`,
+    'gatsby-plugin-robots-txt',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://dev.rootstock.io',
+        sitemap: 'https://dev.rootstock.io/sitemap.xml',
+        policy: [{userAgent: '*', allow: '/', disallow: '/rif/identity'}]
+      }
+    },
     {
       // gatsby-plugin-sitemap version is 3.0.0 because of https://stackoverflow.com/questions/69452298/problem-with-sitemap-generation-in-gatsby-js
       resolve: "gatsby-plugin-sitemap",
