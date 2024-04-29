2a9b40c3f96b3bb1182f62f7f0eaca97ec8e3797
Owanate Amachree <amachreeowanate@gmail.com>
2024-04-29 12:03:32 +0200

    update announcement in before-content marquee

--- src/components/before-content/component-two/index.tsx
+++ src/components/before-content/component-two/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ComponentTwo = () => {
     return(
-        <a href="https://blog.rootstock.io/noticia/arrowhead-6-1-0-release-what-you-need-to-know/" target="_blank" rel="noopener noreferrer"> 
-      Latest version of RSKj now available on GitHub! While this upgrade is optional, it is recommended that users update their nodes to the recent version (Arrowhead v6.1.0) for an enhanced experience.
+        <a href="https://blog.rootstock.io/noticia/merged-mining-insights-report-q1-2024/" target="_blank" rel="noopener noreferrer"> 
+      Rootstock x Bitcoin: Merged Mining Insights Report | Q4 2024.
       </a>
     )
 }
